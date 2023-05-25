f2f0b86b2225c3caefa7ed2fa01474e1bf5fed91
Nitika <nitika@preciousinfosystem.com>
2023-05-25 20:33:39 +0530

    add mailer

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   belongs_to :building
   has_many :items
   
+  validates :description, length: { maximum: 40 ,too_long: "%{count} characters is the maximum allowed"}
   validates :name, uniqueness: true
 
 end
