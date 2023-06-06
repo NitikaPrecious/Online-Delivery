4cf52c5fa4c1570b09f7b0f0508d1233a2ff1165
Nitika <nitika@preciousinfosystem.com>
2023-06-06 15:40:32 +0530

    add ..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@
 
 class HomeController < ApplicationController
 
-  def index
-  end
-
   def search
     item_id = params[:item_id]
     restaurant_id = params[:restaurant_id]
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,4 @@ class ItemsController < ApplicationController
   def show
     @item=Item.find(params[:id])
   end
-
-  def new; end
-
-  def edit; end
-
-  def create; end
-
-  def update; end
-
-  def destroy; end
 end
